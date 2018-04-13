501585279c9eea008afac3ad2c00300b4c0aa81d
“Richard Middleton” <Richard@richardmiddleton.me>
2018-04-13 16:24:43 +0200

    score and walls

--- pacman.js
+++ pacman.js
@@ -13,26 +13,48 @@ function Pacman () {
         switch(keyCode) {
             case UP_ARROW:
                 if(world[this.location.y-1][this.location.x] !== '0') {
+                    this.img = pacmanUp;
+                    if(world[this.location.y-1][this.location.x] !== '1') {
+                        score++;
+                    }
                     world[this.location.y][this.location.x] = '1';
                     this.location.y = this.location.y -1;
                 }
                 break;
             case DOWN_ARROW:
                 if(world[this.location.y+1][this.location.x] !== '0') {
+                    this.img = pacmanDown;
+                    if(world[this.location.y+1][this.location.x] !== '1') {
+                        score++;
+                    }
                     world[this.location.y][this.location.x] = '1';
                     this.location.y = this.location.y +1;
                 }
                 break;
             case LEFT_ARROW:
                 if(world[this.location.y][this.location.x -1] !== '0') {
+                    this.img = pacmanLeft;
+                    if(world[this.location.y][this.location.x -1] !== '1') {
+                        score++;
+                    }
                     world[this.location.y][this.location.x] = '1';
                     this.location.x = this.location.x -1;
+                }
+                if (pacman.location.x < 0){
+                    pacman.location.x = 27;
                 }
                 break;
             case RIGHT_ARROW:
                 if(world[this.location.y][this.location.x +1] !== '0') {
+                    this.img = pacmanImg;
+                    if(world[this.location.y][this.location.x+1] !== '1') {
+                        score++;
+                    }
                     world[this.location.y][this.location.x] = '1';
                     this.location.x = this.location.x +1;
+                }
+                if (pacman.location.x >27){
+                    pacman.location.x = 0;
                 }
                 break;
             default:
--- sketch.js
+++ sketch.js
@@ -1,10 +1,17 @@
 var pacmanImg;
+var pacmanLeft;
+var pacmanUp;
+var pacmanDown;
 var pacman;
+var score = 0;
 
 var cell = 20;
 
 function preload() {
     pacmanImg = loadImage('pacman.png');
+    pacmanLeft = loadImage('pacman-left.png');
+    pacmanUp = loadImage('pacman-up.png');
+    pacmanDown = loadImage('pacman-down.png');
 }
 
 function setup() {
@@ -21,7 +28,11 @@ function draw() {
     drawWorld();
     food();
     pacman.show();
-    score();
+    textSize(30);
+    fill(255);
+    strokeWeight(8);
+    stroke(0);
+    text(score, 10, 30);
 }
 
 function keyPressed() {
